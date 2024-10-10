d41b23b5b0d0c3d4883f951cc2b22b41025159dc
Nuno Rodrigues <nunomr@gmail.com>
2024-10-10 15:31:09 +0000

    finish iterator tests

--- src/render/iterator.test.ts
+++ src/render/iterator.test.ts
@@ -3,6 +3,25 @@ import { assertEquals } from "jsr:@std/assert@1.0.6";
 import { isIterable, iterator } from "./iterator.ts";
 
 Deno.test(`iterator()`, async (t) => {
+  const testCases = [
+    { input: [], expected: [] },
+    { input: [1, 2, 3], expected: [1, 2, 3] },
+    { input: [`1`, `2`, `3`], expected: [`1`, `2`, `3`] },
+    { input: [], expected: [] },
+    { input: new Set([1, 2, 3]), expected: [1, 2, 3] },
+    { input: new Set([`1`, `2`, `3`]), expected: [`1`, `2`, `3`] },
+    { input: new Map(), expected: [] },
+    { input: new Map([[`1`, 1], [`2`, 2], [`3`, 3]]), expected: [[`1`, 1], [`2`, 2], [`3`, 3]] },
+    { input: iterator([]), expected: [] },
+    { input: iterator([1, 2, 3]), expected: [1, 2, 3] },
+    { input: iterator([`1`, `2`, `3`]), expected: [`1`, `2`, `3`] },
+  ];
+
+  for (const testCase of testCases) {
+    await t.step(`Test case "${JSON.stringify(testCase.input)}"`, () => {
+      assertEquals([...iterator(testCase.input)], testCase.expected);
+    });
+  }
 });
 
 Deno.test(`isIterable()`, async (t) => {
@@ -11,13 +30,17 @@ Deno.test(`isIterable()`, async (t) => {
     { input: null, expected: false },
     { input: 0, expected: false },
     { input: 1, expected: false },
+    { input: 1.23, expected: false },
+    { input: true, expected: false },
+    { input: false, expected: false },
     { input: ``, expected: false },
+    { input: `abc`, expected: false },
     { input: {}, expected: false },
     { input: new Object(), expected: false },
     { input: [], expected: true },
     { input: [1, 2, 3], expected: true },
     { input: iterator([]), expected: true },
-    { input: new Set(), expected: false },
+    { input: new Set(), expected: true },
     { input: new Map(), expected: true },
   ];
 
