7fc3669322f7e12c1b23756a0da5725472a69fda
Nuno Rodrigues <nunomr@gmail.com>
2025-01-16 19:19:27 +0000

    feat: generate diff png's for multiple variations
    Variations:
    - Mobile (portrait mode cell phone) resolution.
    - HD (desktop) resolution.
    - Dark prefered color scheme.
    - Light prefered color scheme.

--- src/html-to-image/index.js
+++ src/html-to-image/index.js
@@ -1,6 +1,12 @@
 const path = require("node:path");
 const puppeteer = require("puppeteer");
 
+const HD_RESOLUTION = { width: 1920, height: 1080 };
+const MOBILE_RESOLUTION = { width: 412, height: 915, isMobile: true }; // Pixel 7 portrait mode
+
+const DARK_COLOR_SCHEME = `dark`;
+const LIGHT_COLOR_SCHEME = `light`;
+
 async function scroll(page) {
   return await page.evaluate(async () => {
     return await new Promise((resolve, _reject) => {
@@ -26,13 +32,14 @@ function fileExtension(filename) {
   return extension;
 }
 
-async function htmlToImage(url, imageFilename) {
+async function htmlToImage(url, imageFilename, viewport = HD_RESOLUTION, prefersColorScheme = LIGHT_COLOR_SCHEME) {
   const browser = await puppeteer.launch({ headless: true });
 
   try {
     const page = await browser.newPage();
 
-    await page.setViewport({ width: 1280, height: 1024 });
+    await page.setViewport(viewport);
+    await page.emulateMediaFeatures([{ name: "prefers-color-scheme", value: prefersColorScheme }]);
     await page.goto(url, { waitUntil: ["load", "domcontentloaded"] });
     await scroll(page);
     await page.screenshot({
@@ -46,9 +53,18 @@ async function htmlToImage(url, imageFilename) {
 }
 
 async function main() {
+  const currentUrl = `https://cv.nunorodrigues.tech/`;
+  const newUrl = `file://${path.resolve("./generated/cv.html")}`;
+
   await Promise.all([
-    htmlToImage(`https://cv.nunorodrigues.tech/`, `generated/current.png`),
-    htmlToImage(`file://${path.resolve("./generated/cv.html")}`, `generated/this.png`),
+    htmlToImage(currentUrl, `generated/current.hd.dark.png`, HD_RESOLUTION, DARK_COLOR_SCHEME),
+    htmlToImage(currentUrl, `generated/current.hd.light.png`, HD_RESOLUTION, LIGHT_COLOR_SCHEME),
+    htmlToImage(currentUrl, `generated/current.mobile.dark.png`, MOBILE_RESOLUTION, DARK_COLOR_SCHEME),
+    htmlToImage(currentUrl, `generated/current.mobile.light.png`, MOBILE_RESOLUTION, LIGHT_COLOR_SCHEME),
+    htmlToImage(newUrl, `generated/new.hd.dark.png`, HD_RESOLUTION, DARK_COLOR_SCHEME),
+    htmlToImage(newUrl, `generated/new.hd.light.png`, HD_RESOLUTION, LIGHT_COLOR_SCHEME),
+    htmlToImage(newUrl, `generated/new.mobile.dark.png`, MOBILE_RESOLUTION, DARK_COLOR_SCHEME),
+    htmlToImage(newUrl, `generated/new.mobile.light.png`, MOBILE_RESOLUTION, LIGHT_COLOR_SCHEME),
   ]);
 }
 
